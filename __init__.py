c230aeb111daf7a8838b5e6984539aefd1f3c841
deepaks25bai108-create <deepaks25bai108@skasc.ac.in>
2026-03-23 21:31:55 +0530

    Initialize package with module docstring
    Add module docstring and initialize package.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
+"""
+__init__.py for My Project.
+
+This package initializes the modules and imports necessary functions for the project.
+"""
+
 from .emotion_detection import emotion_detector
+
+__all__ = ['emotion_detector']
+
